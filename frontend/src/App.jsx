435802fee0f039c6970f72e79f793c80972ee262
Kamil678 <kamilpigulak@interia.pl>
2024-03-09 16:08:01 +0100

    add header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,35 +6,37 @@ import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route
           path="/"
           element={<Home />}
-        ></Route>
+        />
         <Route
           path="/sign-in"
           element={<Signin />}
-        ></Route>
+        />
         <Route
           path="/sign-up"
           element={<Signup />}
-        ></Route>
+        />
         <Route
           path="/about"
           element={<About />}
-        ></Route>
+        />
         <Route
           path="/dashboard"
           element={<Dashboard />}
-        ></Route>
+        />
         <Route
           path="/projects"
           element={<Projects />}
-        ></Route>
+        />
       </Routes>
     </BrowserRouter>
   );
