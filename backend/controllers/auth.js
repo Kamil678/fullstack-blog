48abf3594a07379acdf99e575ef5096177cd63bb
Kamil678 <kamilpigulak@interia.pl>
2024-03-20 15:37:34 +0100

    create signin controller

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,11 +1,12 @@
 import User from "../models/user.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res, next) => {
   const { email, username, password } = req.body;
 
-  if (!email || !username || !password || email === "" || username === "" || password === "") {
+  if (!email || !username || !password) {
     next(errorHandler(400, "All fields are required!"));
   }
 
@@ -20,3 +21,32 @@ export const signup = async (req, res, next) => {
     next(err);
   }
 };
+
+export const signin = async (req, res, next) => {
+  const {email, password} = req.body
+
+  if(!username || !password){
+    next(errorHandler(400, 'Wszystkie pola są wymagane!'))
+  }
+
+  try{
+    const user = User.findOne({email})
+
+    if(!user){
+      return next(errorHandler(404, 'Nie znaleziono takiego użytkownika.'))
+    }
+
+    const password = bcryptjs.compareSync(password, user.password)
+
+    if(!password){
+      return next(errorHandler(400, 'Niepoprawne hasło.'))
+    }
+
+    const token = jwt.sign({userId:user._id, username:user.username}, "somesupersecretsecret", {expiresIn:'1h'});
+    const {password:pass, ...responseUser} = user._doc
+
+    res.status(200).cookie('token', token, {httpOnly:true, SameSite:'strict'}).json(responseUser);
+  } catch(err){
+    next(err);
+  }
+}
--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,6 @@ const app = express();
 app.use(express.json());
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000!!!!!");
   mongoose
     .connect(process.env.MONGO_CONNECTION)
     .then(() => {
