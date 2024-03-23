6b05b566a15436356845a5f4272e693f28ba78c4
Kamil Pigulak <kamilpigulak@interia.pl>
2024-03-23 16:23:00 +0100

    update header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,22 +1,18 @@
-import { Button, Navbar, TextInput } from "flowbite-react";
+import { Avatar, Button, Dropdown, DropdownDivider, DropdownHeader, DropdownItem, Navbar, TextInput } from "flowbite-react";
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
+import { FaMoon, FaUser } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { useSelector } from "react-redux";
 import Logo from "./Logo";
 
 export default function Header() {
   const path = useLocation().pathname;
+  const { user } = useSelector((state) => state.user);
 
   return (
     <Navbar className="border-b-2">
-      {/* <Link
-        to="/"
-        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white "
-      >
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white mr-1">Fullstack</span>
-        Blog
-      </Link> */}
       <Logo textSize="text-sm sm:text-xl" />
       <form>
         <TextInput
@@ -41,14 +37,43 @@ export default function Header() {
         >
           <FaMoon />
         </Button>
-        <Link to="/sign-in">
-          <Button
-            gradientDuoTone="purpleToBlue"
-            outline
+        {user ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar
+                alt="User avatar"
+                img={user.picture}
+              />
+            }
           >
-            Zaloguj się
-          </Button>
-        </Link>
+            <DropdownHeader>
+              <div className="flex gap-2 items-center">
+                <FaUser className="w-4 h-4" />
+                <span className="block text-sm">{user.username}</span>
+              </div>
+              <div className="flex gap-2 items-center">
+                <MdEmail className="w-4 h-4" />
+                <span className="block text-sm font-medium truncate">{user.email}</span>
+              </div>
+            </DropdownHeader>
+            <Link to={"/dashboard?tab=profile"}>
+              <DropdownItem>Profil</DropdownItem>
+            </Link>
+            <DropdownDivider />
+            <DropdownItem>Wyloguj się</DropdownItem>
+          </Dropdown>
+        ) : (
+          <Link to="/sign-in">
+            <Button
+              gradientDuoTone="purpleToBlue"
+              outline
+            >
+              Zaloguj się
+            </Button>
+          </Link>
+        )}
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
