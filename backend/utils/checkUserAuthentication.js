f040334ffe05dca46f11aafb0250bb7826beb509
Kamil Pigulak <kamilpigulak@interia.pl>
2024-07-25 08:19:49 +0200

    reafctor

--- backend/utils/checkUserAuthentication.js
+++ backend/utils/checkUserAuthentication.js
@@ -3,34 +3,17 @@ import { errorHandler } from "./error.js";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
-  console.log(token);
 
   if (!token) {
-    console.log("BRak tokena");
-    return next(
-      errorHandler(
-        401,
-        "User is unauthorized.",
-        "Użytkownik jest niezautoryzowany."
-      )
-    );
+    return next(errorHandler(401, "User is unauthorized.", "Użytkownik jest niezautoryzowany."));
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    console.log(err);
     if (err) {
-      console.log("Zły tokrn");
-      return next(
-        errorHandler(
-          401,
-          "User is unauthorized.",
-          "Użytkownik jest niezautoryzowany."
-        )
-      );
+      return next(errorHandler(401, "User is unauthorized.", "Użytkownik jest niezautoryzowany."));
     }
 
     req.user = user;
-    console.log(req.user, user);
     next();
   });
 };
