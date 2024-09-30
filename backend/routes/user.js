2f15fafab257086a5e65c65722ed911eb22ed9a1
Kamil678 <kamilpigulak@interia.pl>
2024-09-30 20:30:00 +0200

    feat: add get users api route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { testUser, updateUser, deleteUser } from "../controllers/user.js";
+import { updateUser, deleteUser, getUsers } from "../controllers/user.js";
 import { verifyToken } from "../utils/checkUserAuthentication.js";
 
 const router = express.Router();
 
-router.get("/test", testUser);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
+router.get("/getusers", verifyToken, getUsers);
 
 export default router;
