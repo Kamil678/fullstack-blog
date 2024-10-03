0ce649f78fb5f473c9856dad925434589423dfe5
Kamil678 <kamilpigulak@interia.pl>
2024-10-03 08:35:51 +0200

    feat: create call to action box

--- frontend/src/pages/Post.jsx
+++ frontend/src/pages/Post.jsx
@@ -1,6 +1,7 @@
 import { Button, Spinner } from "flowbite-react";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
 
 export default function Post() {
   const { postSlug } = useParams();
@@ -66,6 +67,9 @@ export default function Post() {
         dangerouslySetInnerHTML={{ __html: post && post.content }}
         className="p-3 max-w-2xl mx-auto w-full post-content"
       ></div>
+      <div className="max-w-4xl mx-auto w-full">
+        <CallToAction />
+      </div>
     </main>
   );
 }
